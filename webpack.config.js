db75945d4f2be3e3c078e7c940de26c546126a30
Soobarna <subarnapokharel252@gmail.com>
2023-11-05 08:34:17 +0545

    Description-section-completed

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,30 @@ const autoprefixer = require('autoprefixer')
 const path=require('path')
 const postCSSplugins=[
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
 ]
 module.exports={
+   
     entry:'./app/assets/scripts/App.js',
     output:{
         filename:'bundled.js',
         path:path.resolve(__dirname,'app')
     },
+    devServer:{
+        watchFiles:["app/**/*.html"],
+        static:{
+            directory:path.join(__dirname,"app"),
+            watch:false
+        },
+        hot:true,
+        port:3000,
+        
+    },
     mode:'development',
-    watch:true,
+    
     module:{
         rules:[
             {
